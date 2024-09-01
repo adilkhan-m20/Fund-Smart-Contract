aa91e9b0683e7144e3612bf05ae5ab548c659903
Adil Khan <160357389+adilkhan-m20@users.noreply.github.com>
2024-09-01 22:36:33 +0530

    Update PriceConverter.sol

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -4,13 +4,6 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getVersion() internal view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
-        );
-        return priceFeed.version();
-    }
-
     function getPrice(
         AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
